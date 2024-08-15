bbd7071e10a2da34c8be6d6f92f2d99db113dd35
awtik <dreamik1102@gmail.com>
2024-08-16 00:00:43 +0300

    Added comments

--- cryptLib.py
+++ cryptLib.py
@@ -5,11 +5,13 @@ class Cipher:
     def __init__(self):
         pass
     def encrypter(self, text):
+        """Method for encrypt"""
         cipher_key = Fernet.generate_key()
         cipher = Fernet(cipher_key)
         encrypted_text = cipher.encrypt(text)
         return cipher_key, encrypted_text
     def decrypter(self, key, text):
+        """Method for decrypt"""
         cipher = Fernet(key)
         decrypted_text = cipher.decrypt(text)
         return decrypted_text
--- crypter.py
+++ crypter.py
@@ -1,30 +1,49 @@
 import os
 from cryptLib import Cipher
 
-c = Cipher()
+c = Cipher() # Initialization crypter class
+os.system('cls') # Clear console
 def cryption():
-    os.system('cls')
-    text = input('Insert text to encrypt:\n: ')
-    text = text.encode()
-    os.system('cls')
-    key, encriptedText = c.encrypter(text)
-    print(f'''Key to decrypt: {key}\nEncrypted text: {encriptedText}''')
+    """Function for crypt"""
+    os.system('cls') # Clear console
+    text = input('Insert text to encrypt:\n: ') # Get text for crypt
+    text = text.encode() # Byting user's text
+    os.system('cls') # Clear console
+    key, encriptedText = c.encrypter(text) # Crypting text
+    print(f'''Key to decrypt: {key}\nEncrypted text: {encriptedText}''') # Print result
     wait = input('\nSuccesful! You can copy this data!\nTo continue press Enter!')
+    os.system('cls') # Clear console
+
 def decryption():
-    os.system('cls')
-    text = input('Insert text to decrypt:\n: ')
-    text = text.encode()
-    key = input('Insert key to decrypt:\n: ')
-    key = key.encode()
-    os.system('cls')
-    print(c.decrypter(key, text))
+    """Function for decrypt"""
+    os.system('cls') # Clear console
+    text = input('Insert text to decrypt:\n: ') # Get text for decrypt
+    text = text.encode() # Byting user's text
+    key = input('Insert key to decrypt:\n: ') # Get key for decrypt
+    key = key.encode() # Byting user's text
+    os.system('cls') # Clear console
+    print(c.decrypter(key, text)) # Print result
     wait = input('\nSuccesful! You can copy this data!\nTo continue press Enter!')
+    os.system('cls') # Clear console
+
+def commands():
+    """Function for showing commands list"""
+    os.system('cls') # Clear console
+    print("""             Command list
+       crypt - Crypt your text
+       decrypt - Decrypt your text with your cryption key
+       exit - Close program
+       help - Show this text\n""")
+
+commands()
 while(1):
-    os.system('cls')
-    command = input('Insert command:\n: ')
+    command = input('Insert command:\n: ') # Get command from user
     if command == 'crypt':
         cryption()
     elif command == 'decrypt':
         decryption()
+    elif command == 'help':
+        commands()
     elif command == 'exit':
+        os.system('cls') # Clear console
         exit()
